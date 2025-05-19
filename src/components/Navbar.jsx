bb5311ed095d812b4916fd0fa3343089203e751d
Leo <leiw@berkeley.edu>
2025-05-18 21:47:17 -0700

    Fixed scrolling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,25 @@
 import { DarkModeToggle } from "./DarkModeToggle";
+import { Link as ScrollLink } from "react-scroll";
 
 export const Navbar = () => {
     return (
             <nav className="fixed top-0 left-0 right-0 bg-white shadow z-10 dark:bg-gray-900 dark:text-white">
                 <ul className="flex justify-center space-x-6 p-4 font-medium">
-                <li><a href="#about" className="hover:text-blue-500">About</a></li>
-                <li><a href="#projects" className="hover:text-blue-500">Projects</a></li>
-                <li><a href="#more" className="hover:text-blue-500">Etc</a></li>
+                <li>
+                    <ScrollLink to="about" smooth={true} duration={200} offset={-80} className="cursor-pointer hover:text-blue-500">
+                        About
+                    </ScrollLink>
+                </li>
+                <li>
+                    <ScrollLink to="projects" smooth={true} duration={200} offset={-80} className="cursor-pointer hover:text-blue-500">
+                        Projects
+                    </ScrollLink>
+                </li>
+                <li>
+                    <ScrollLink to="more" smooth={true} duration={500} offset={-80} className="cursor-pointer hover:text-blue-500">
+                        Etc
+                    </ScrollLink>
+                </li>
                 <li><DarkModeToggle /></li>
                 </ul>
             </nav>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,6 @@
 export const NotFound = () => {
     return <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
     <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-    <a href="/" className="mt-4 text-blue-500 underline">Go Home</a>
+    <a href="/portfolio" className="mt-4 text-blue-500 underline">Go Home</a>
   </div>
 }
